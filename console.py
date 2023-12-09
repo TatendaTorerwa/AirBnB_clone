ec9bf3b77ab52624335035ded6d9766c9727feec
Tanagends <tanagends@gmail.com>
2023-12-09 02:04:30 +0200

    all

--- console.py
+++ console.py
@@ -2,7 +2,8 @@
 """Module for the console"""
 import cmd
 from models.base_model import BaseModel
-from models.engine import file_storage
+from models.engine.file_storage import FileStorage
+from models.__init__ import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -41,16 +42,51 @@ class HBNBCommand(cmd.Cmd):
         if not line:
             print("** class name missing **")
             return
+        
+        args = line.split()
         try:
-            if not isinstance(line.split()[0](), object):
+            if not isinstance(args[0](), object):
                 raise NameError
         except NameError:
             print("** class doesn't exist **")
             return
-        if len(line.split()) < 2:
+
+        if len(args) < 2:
             print("** instance id missing **")
             return
-        
+
+        try:
+            print(storage.__objects[args[0] + "." + args[1]])
+        except KeyError:
+            print("** no instance found **")
+
+    def do_destroy(self, line):
+        """Deletes an instance based on class name and ID"""
+        if not line:
+            print("** class name missing **")
+            return
+        args = line.split()
+        try:
+            if not isinstance(args[0](), object):
+                raise NameError
+        except NameError:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+        try:
+            del storage.__objects[args[0] + "." + args[1]]
+        except KeyError:
+            print("** no instance found **")
+
+    def do_all(self, line):
+        """Prints a string representation off all
+        objects based, or not on the class name"""
+        if not line:
+            for value in storage.__objects.values():
+                print(value)
+        elif:
+
 
 
 if __name__ == "__main__":
