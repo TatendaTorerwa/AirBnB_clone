a70a44b88125e5ce119d14a99b5ddfae02594814
TatendaTorerwa <ttorerwa01@gmail.com>
2023-12-09 13:32:08 -0800

    3. BaseModel.

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3
-import models
-from uuid import uuid
+#!/usr/bin/python
+import modules
+import uuid
 from datetime import datetime
 
 class BaseModel:
@@ -13,18 +13,18 @@ class BaseModel:
                 else:
                     setattr(self, key, value)
         else:
-        self.id = str(uuid.uuid4())
-        self created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+            self.id = str(uuid.uuid4())
+            self created_at = datetime.now()
+            self.updated_at = datetime.now()
 
         def __str__(self):
-                 return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+            return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
         def save(self):
-            updated_at = datetime.datetime.now()
+            self.updated_at = datetime.now()
 
         def to_dict(self):
-            instance_dict = self.__dict__.copy
+            instance_dict = self.__dict__.copy()
             instance_dict['__class__'] = self.__class__.__name__
             instance_dict['created_at'] = self.created_at.isoformat()
             instance_dict['updated_at'] = self.updated_at.isoformat()
