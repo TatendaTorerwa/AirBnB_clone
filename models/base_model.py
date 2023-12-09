aa34be6c3dba44b926e98fc51ed1973e8e471bcd
Tanagends <tanagends@gmail.com>
2023-12-10 00:54:11 +0200

    update

--- models/base_model.py
+++ models/base_model.py
@@ -23,7 +23,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            #models.storage.new(self)
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
@@ -31,7 +31,7 @@ class BaseModel:
     def save(self):
         """Updates the the attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
-        models.storage.save()
+        #models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary, __dict__ of the instance"""
