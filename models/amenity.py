06be484b73769abdc09f9395ba9fd7df485a9417
TatendaTorerwa <ttorerwa01@gmail.com>
2023-12-10 06:34:12 -0800

    All the changes made.

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""Defines the Amenity class."""
 
 from models.base_model impot BaseModel
 
 class Amenity(BaseModel):
+    ""Represents an amenity.
+
+    Attributes:
+        name (str): The name of the amenity.
+    """
 
     name = ""
--- models/base_model.py
+++ models/base_model.py
@@ -3,29 +3,18 @@
 import models
 from uuid import uuid4
 from datetime import datetime
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 import models
->>>>>>> a484b7494f37c12cec7cd1035c0e9c95c99ac6cc
-=======
 import models import storage
->>>>>>> ebc5616b9d1634575afeaa9d8f1a6618aed3eefd
 
 
 class BaseModel:
     """Represents the BaseModel of the HBnB project."""
 
     def __init__(self, *args, **kwargs):
-<<<<<<< HEAD
-<<<<<<< HEAD
-        """Initialize a new BaseModel.
-=======
+        """Initialize a new BaseModels.
         if kwargs:
-=======
         """Initializes instance variables"""
         if kwargs is not None and kwargs != {}:
->>>>>>> ebc5616b9d1634575afeaa9d8f1a6618aed3eefd
             for name, value in kwargs.items():
                 match name:
                     #case "__class__":
@@ -37,12 +26,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-<<<<<<< HEAD
             models.storage.new(self)
->>>>>>> a484b7494f37c12cec7cd1035c0e9c95c99ac6cc
-=======
             #models.storage.new(self)
->>>>>>> ebc5616b9d1634575afeaa9d8f1a6618aed3eefd
 
         Args:
             *args (any): Unused.
@@ -62,7 +47,7 @@ class BaseModel:
             models.storage.new(self)
 
     def save(self):
-<<<<<<< HEAD
+
         """Update updated_at with the current datetime."""
         self.updated_at = datetime.today()
         models.storage.save()
@@ -83,7 +68,7 @@ class BaseModel:
         """Return the print/str representation of the BaseModel instance."""
         clname = self.__class__.__name__
         return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
-=======
+
         """Updates the the attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
         #models.storage.save()
@@ -95,4 +80,3 @@ class BaseModel:
         instance_dict['updated_at'] = str(self.updated_at.isoformat())
         instance_dict['created_at'] = str(self.created_at.isoformat())
         return instance_dict
->>>>>>> a484b7494f37c12cec7cd1035c0e9c95c99ac6cc
