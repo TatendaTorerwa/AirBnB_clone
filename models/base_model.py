d4076680ed9917e23031da2f2bffa845298c1d26
Tanagends <tanagends@gmail.com>
2023-12-10 20:50:37 +0200

    Airbnb

--- models/base_model.py
+++ models/base_model.py
@@ -3,21 +3,14 @@
 import models
 from uuid import uuid4
 from datetime import datetime
-<<<<<<< HEAD
 from models import storage
-=======
-import models
-import models import storage
->>>>>>> 06be484b73769abdc09f9395ba9fd7df485a9417
 
 
 class BaseModel:
     """Represents the BaseModel of the HBnB project."""
 
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModels.
-        if kwargs:
-        """Initializes instance variables"""
+        """Initialize a new BaseModels."""
         if kwargs is not None and kwargs != {}:
             for name, value in kwargs.items():
                 match name:
@@ -30,47 +23,13 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-<<<<<<< HEAD
             storage.new(self)
-=======
-            models.storage.new(self)
-            #models.storage.new(self)
->>>>>>> 06be484b73769abdc09f9395ba9fd7df485a9417
-
-        Args:
-            *args (any): Unused.
-            **kwargs (dict): Key/value pairs of attributes.
-        """
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tform)
-                else:
-                    self.__dict__[k] = v
-        else:
-            models.storage.new(self)
 
     def save(self):
 
         """Update updated_at with the current datetime."""
         self.updated_at = datetime.today()
         models.storage.save()
-
-    def to_dict(self):
-        """Return the dictionary of the BaseModel instance.
-
-        Includes the key/value pair __class__ representing
-        the class name of the object.
-        """
-        rdict = self.__dict__.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
 
     def __str__(self):
         """Return the print/str representation of the BaseModel instance."""
